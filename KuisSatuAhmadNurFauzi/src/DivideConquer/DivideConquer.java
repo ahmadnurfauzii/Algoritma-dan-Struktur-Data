9946678c9c0bf77f76af20e04d9e7e2e81edea1b
ahmadnurfauzii <58428390+ahmadnurfauzii@users.noreply.github.com>
2020-03-05 10:31:24 +0700

    Add files via upload

---
+++ KuisSatuAhmadNurFauzi/src/DivideConquer/DivideConquer.java
@@ -0,0 +1,20 @@
+package DivideConquer;
+
+public class DivideConquer {
+
+    public static void main(String[] args) {
+        int n = 4;
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+
+    static void towerOfHanoi(int numDisc, char source, char destination, char spare) {
+        if (numDisc == 1) {
+            System.out.println("Pindah 1 gelang dari tower " + source + " ke tower " + destination);
+            return;
+        }
+        towerOfHanoi(numDisc - 1, source, spare, destination);
+        System.out.println("Pindah gelang " + numDisc + " dari tower " + source + " ke tower " + destination);
+        towerOfHanoi(numDisc - 1, spare, destination, source);
+    }
+}
+    
