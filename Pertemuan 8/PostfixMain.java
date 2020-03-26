d87ec3d57e9f9db135fd039ff530ced7d72d7c68
ahmadnurfauzii <58428390+ahmadnurfauzii@users.noreply.github.com>
2020-03-26 10:45:18 +0700

    Add files via upload

---
+++ Pertemuan 8/PostfixMain.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class PostfixMain {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String P, Q;
+        System.out.println("Masukan ekspresi matematika: ");
+        Q = sc.nextLine();
+        Q = Q.trim();
+        Q = Q + ")";
+        
+        int total = Q.length();
+        
+        Postfix post = new Postfix(total);
+        P = post.konversi(Q);
+        System.out.println("Postfix: " + P);
+    }
+    
+}
