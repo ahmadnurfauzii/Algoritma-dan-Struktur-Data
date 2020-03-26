ee979c7867175e06f06489c0835459837e22aea2
ahmadnurfauzii <58428390+ahmadnurfauzii@users.noreply.github.com>
2020-03-26 10:43:54 +0700

    Add files via upload

---
+++ Pertemuan 7/src/tugas4/Penduduk.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Penduduk {
+    
+        public int nik;
+    public String nama, alamat, jenisK;
+
+    public Penduduk(int NIK, String nm, String al, String jk) {
+        nik = NIK;
+        nama = nm;
+        alamat = al;
+        jenisK = jk;
+    }
+}
+
